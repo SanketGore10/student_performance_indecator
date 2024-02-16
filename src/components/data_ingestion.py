5c6afef67db01b94a759f8bcfaaf2845281bfaa9
Sanket Gore <sanketgore1998@gmail.com>
2024-02-16 19:31:56 +0530

    started documentation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+#creating this class to store the splited data 
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str = os.path.join('artifact', "train.csv")
